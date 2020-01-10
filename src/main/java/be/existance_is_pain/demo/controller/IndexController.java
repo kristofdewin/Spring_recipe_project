d0ada21b29e703bc8af5343260ba61a9f3aa99b9
kristofdewin <kristofdewin1982@gmail.com>
2020-01-10 13:32:04 +0100

    initial commit

---
+++ src/main/java/be/existance_is_pain/demo/controller/IndexController.java
@@ -0,0 +1,13 @@
+package be.existance_is_pain.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
---
+++ src/test/java/be/existance_is_pain/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package be.existance_is_pain.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
